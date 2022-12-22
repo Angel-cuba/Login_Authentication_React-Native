536f1c412a066c848ddbf6e29ce0752df5967ed6
Mac_Developer <araozangel842@gmail.com>
2022-12-22 14:34:45 +0200

    Add shadow styles to theme object

--- config/theme.js
+++ config/theme.js
@@ -1,8 +1,14 @@
 export const colors = {
   white: '#F3FBFFD4',
-  primary : '#FBFBFBC6',
+  primary : '#FBFBFB',
   secondary : '#BC9C9C80',
   tertiary : '#FDFDFD78',
   tint : '#5E2C2C',
   accent : '#FF4D00'
 }
+
+export const shadows = {
+  shadowDark: '#0000000C',
+  shadowLight: '#FFFFFF58',
+  shadowAccent: '#FF4D0075',
+  }
