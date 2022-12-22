9f351248efb7bd9cfce51676bad0d77db5ad1486
Mac_Developer <araozangel842@gmail.com>
2022-12-22 18:18:40 +0200

    Add more colors

--- config/theme.js
+++ config/theme.js
@@ -1,5 +1,6 @@
 export const colors = {
   white: '#F3FBFFD4',
+  black: '#000000',
   primary : '#FBFBFB',
   secondary : '#BC9C9C80',
   tertiary : '#FDFDFD78',
@@ -8,6 +9,7 @@ export const colors = {
 }
 
 export const shadows = {
+  shadowBlack: '#00000095',
   shadowDark: '#0000000C',
   shadowLight: '#FFFFFF58',
   shadowAccent: '#FF4D0075',
