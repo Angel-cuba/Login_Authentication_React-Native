577206aa0180c2013eae1b6da3e6cd76d862b170
Mac_Developer <araozangel842@gmail.com>
2022-12-26 00:24:17 +0200

    Add default theme and some dummy data

--- config/theme.js
+++ config/theme.js
@@ -1,16 +1,39 @@
 export const colors = {
   white: '#F3FBFFD4',
   black: '#000000',
-  primary : '#FBFBFB',
-  secondary : '#BC9C9C80',
-  tertiary : '#FDFDFD78',
-  tint : '#5E2C2C',
-  accent : '#FF4D00'
-}
+  primary: '#FBFBFB',
+  secondary: '#BC9C9C80',
+  tertiary: '#FDFDFD78',
+  tint: '#5E2C2C',
+  accent: '#FF4D00',
+};
 
 export const shadows = {
   shadowBlack: '#00000095',
   shadowDark: '#0000000C',
   shadowLight: '#FFFFFF58',
-  shadowAccent: '#FF4D0075',
-  }
+  shadowAccent: '#FC9E7775',
+};
+
+export const defaultTheme = {
+  lightTheme: {
+    white: '#F3FBFFD4',
+    black: '#000000',
+    shadowBlack: '#3636364E',
+    primary: '#FBFBFB',
+    secondary: '#BC9C9C80',
+    tertiary: '#FDFDFD78',
+    tint: '#5E2C2C',
+    accent: '#B93800',
+  },
+  darkTheme: {
+    white: '#1A1B1CD4',
+    black: '#F0E9E9',
+    shadowBlack: '#F0E9E927',
+    primary: '#3E3E3E',
+    secondary: '#121111',
+    tertiary: '#1C1C1C78',
+    tint: '#DA9F9F',
+    accent: '#FF2F00DC',
+  },
+};
